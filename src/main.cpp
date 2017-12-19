d9b99bda4cdbe79427f4900820c9eeedffdddbf1
Anton Krug <anton.krug@microsemi.com>
2017-12-19 15:52:51 +0000

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+/*******************************************************************
+ * (c) Copyright 2016-2017 Microsemi SoC Products Group. All rights reserved.
+ * Minimal but verbose trap handler
+*/
+
+
+int main() {
+  return 1;
+}
